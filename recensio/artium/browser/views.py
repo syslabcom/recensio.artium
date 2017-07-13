cef7fb8d002a22bf2ecb09534f0c8fb13dfd9e7b
Manuel Reinhardt <reinhardt@syslab.com>
2017-07-13 07:53:08 +0000

    Added PDF cover image, refs #15430

--- recensio/artium/browser/views.py
+++ recensio/artium/browser/views.py
@@ -4,4 +4,4 @@ from recensio.theme.browser.pdfgen import GeneratePdfRecension
 class GeneratePdfRecensionArtium(GeneratePdfRecension):
     """Customized cover page
     """
-    logo_main = '++resource++recensio.artium.images/mainimage_de.png'
+    logo_main = '++resource++recensio.artium.images/logo2_fuer-Deckblatt.jpg'
