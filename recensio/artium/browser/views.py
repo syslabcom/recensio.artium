ecd334792a991b01833c7f2747ae4821f0a0b21a
Manuel Reinhardt <reinhardt@syslab.com>
2019-05-08 15:31:45 +0200

    Homepage boxes: Removed language separation. Also removed italics style from navigation. Refs #FLOW-438

--- recensio/artium/browser/views.py
+++ recensio/artium/browser/views.py
@@ -1,3 +1,4 @@
+from recensio.theme.browser.homepage import HomepageView
 from recensio.theme.browser.pdfgen import GeneratePdfRecension
 
 
@@ -5,3 +6,8 @@ class GeneratePdfRecensionArtium(GeneratePdfRecension):
     """Customized cover page
     """
     logo_main = '++resource++recensio.artium.images/logo2_fuer-Deckblatt.jpg'
+
+
+class ArtiumHomepageView(HomepageView):
+
+    review_languages = [u'']
