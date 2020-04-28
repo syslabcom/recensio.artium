4c6c28f63f2c7f6cdedcec1d834dc197c3a9d57c
Manuel Reinhardt <reinhardt@syslab.com>
2020-04-28 09:36:23 +0200

    Black

--- recensio/artium/browser/views.py
+++ recensio/artium/browser/views.py
@@ -5,9 +5,10 @@ from recensio.theme.browser.pdfgen import GeneratePdfRecension
 class GeneratePdfRecensionArtium(GeneratePdfRecension):
     """Customized cover page
     """
-    logo_main = '++resource++recensio.artium.images/logo2_fuer-Deckblatt.jpg'
+
+    logo_main = "++resource++recensio.artium.images/logo2_fuer-Deckblatt.jpg"
 
 
 class ArtiumHomepageView(HomepageView):
 
-    review_languages = [u'']
+    review_languages = [u""]
